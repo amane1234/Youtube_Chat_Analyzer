231f09c68aee65e9a063b878e9c99f9bb753d483
amane1234 <47608969+amane1234@users.noreply.github.com>
2022-06-21 22:22:43 -0400

    Update youtube_chat_analyzer.py

--- youtube_chat_analyzer.py
+++ youtube_chat_analyzer.py
@@ -5,8 +5,7 @@ from graph_painter import graph_painter
 def analyze():
     video_id: str = input("please type youtube video id: ")
     keywords = list(input(
-        "Type keywords that you want to match. Separate each keyword into spaces. It doesn't matter whether it's lowercase or uppercase.\nex) lol yabe lmao\n* Type enter to keep the default setting(lmao lol lewd yabe)\n" +
-        "매칭시킬 키워드를 입력해주세요. 각 키워드를 띄어쓰기로 구분해서 입력해주세요. 대소문자는 구분하지 않습니다.\n예) lol yabe lmao\n* 기본 설정(lmao lol lewd yabe) 유지하려면 엔터를 치세요.\n").split())
+        "Type keywords that you want to match. Separate each keyword into spaces. It doesn't matter whether it's lowercase or uppercase.\nex) lol yabe lmao\n* Type enter to keep the default setting(lmao lol lewd yabe)\n").split())
     try:
         chat = pytchat.create(video_id=video_id)
     except:
